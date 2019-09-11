54b469671948e99c752e9978fddd4fde4b67a44a
Sriven <331804971@qq.com>
2019-09-11 21:17:41 +0800

    项目结束

---
+++ src/components/news/newsList.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+        <ul class="mui-table-view">
+            <li class="mui-table-view-cell mui-media">
+                <a href="javascript:;">
+                    <img class="mui-media-object mui-pull-left" src="https://t10.baidu.com/it/u=3263918022,3335612071&fm=173&app=49&f=JPEG?w=218&h=146&s=54983F77D843F51B4CE860E80300E022">
+                    <div class="mui-media-body">
+                        <h1>幸福</h1>
+                        <p class='mui-ellipsis'>
+                            <span>发表时间：2012-12-12 12：12：12</span>
+                            <span>点击：0次</span>
+                        </p>
+                    </div>
+                </a>
+            </li>
+            <li class="mui-table-view-cell mui-media">
+                <a href="javascript:;">
+                    <img class="mui-media-object mui-pull-left" src="https://t10.baidu.com/it/u=3263918022,3335612071&fm=173&app=49&f=JPEG?w=218&h=146&s=54983F77D843F51B4CE860E80300E022">
+                    <div class="mui-media-body">
+                        <h1>幸福</h1>
+                        <p class='mui-ellipsis'>
+                            <span>发表时间：2012-12-12 12：12：12</span>
+                            <span>点击：0次</span>
+                        </p>
+                    </div>
+                </a>
+            </li>
+            <li class="mui-table-view-cell mui-media">
+                <a href="javascript:;">
+                    <img class="mui-media-object mui-pull-left" src="https://t10.baidu.com/it/u=3263918022,3335612071&fm=173&app=49&f=JPEG?w=218&h=146&s=54983F77D843F51B4CE860E80300E022">
+                    <div class="mui-media-body">
+                        <h1>幸福</h1>
+                        <p class='mui-ellipsis'>
+                            <span>发表时间：2012-12-12 12：12：12</span>
+                            <span>点击：0次</span>
+                        </p>
+                    </div>
+                </a>
+            </li>
+
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{};
+    },
+    methods:{
+
+    }
+}
+</script>
+
+<style scoped>
+    .mui-table-view li h1{
+        font-size: 12px;
+    }
+    .mui-ellipsis{
+        font-size: 12px;
+        color:#226aff;
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import HomeContainer from './components/tabbar/HomeContainer.vue'
 import MemberContainer from './components/tabbar/MemberContainer.vue'
 import SearchContainer from './components/tabbar/SearchContainer.vue'
 import ShopcartContainer from './components/tabbar/ShopcartContainer.vue'
+import NewsList from './components/news/newsList.vue'
 
 let router = new VueRooter({
     routes:[
@@ -13,6 +14,7 @@ let router = new VueRooter({
         {path:'/search',component:SearchContainer},
         {path:'/cart',component:ShopcartContainer},
         {path:'/member',component:MemberContainer},
+        {path:'/home/newsList',component:NewsList},
     ],
     linkActiveClass:'mui-active'
 });
